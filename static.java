abd02cb47bab190c0af32c78e6a85774a5e10a28
rvibhakar1-sys <rvibhakar1@gmail.com>
2026-09-18 10:21:16 +0530

    add thid

--- static.java
+++ static.java
@@ -1,1 +1,23 @@
 
+public class Static {
+
+    public static void main(String[] args) {
+      Student s1 = new Student();
+        s1.clg = "abc";
+        System.out.println(s1.clg);
+        Student s2 = new Student();   
+        s2.clg = "xyz";
+        System.out.println(s1.clg);
+        System.out.println(s2.clg); 
+        s1.display();
+    }
+}
+
+    class Student {
+       static String clg;
+    
+       public  void display() {
+          System.out.println(" inStudent class ::" + clg);
+       }
+    
+    }
