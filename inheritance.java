7ee2cf62aa7c74a353d83e9c97bfab0cbdf76019
rvibhakar1-sys <rvibhakar1@gmail.com>
2026-09-18 12:22:12 +0530

    add thid

--- inheritance.java
+++ inheritance.java
@@ -1,7 +1,16 @@
 interface inheritance {
-    
+void getArea(int length, int breadth);
+}
 
-    public static void main(String[] args) {
-        
-    }
+class Rectangle implements inheritance {
+public void getArea(int length, int breadth) {
+System.out.println("The area of the rectangle is " + (length * breadth));
+}
+}
+
+class Main {
+public static void main(String[] args) {
+Rectangle r1 = new Rectangle();
+r1.getArea(5, 6);
+}
 }
